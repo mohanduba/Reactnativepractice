502bf3c1f03de7e24df570117a756feb94357afe
mohanduba <mohanraoduba45@gmail.com>
2025-09-12 12:06:22 +0530

    javabackendapi_sep12

--- firstapi_project/src/main/java/com/example/www/controller/FirstController.java
+++ firstapi_project/src/main/java/com/example/www/controller/FirstController.java
@@ -1,6 +1,8 @@
 package com.example.www.controller;
 
 import java.util.List;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,22 +19,24 @@ import com.example.www.model.Firstprj;
 @RestController
 @RequestMapping("/api/exp1/")
 @CrossOrigin(origins = "*")
+@Tag(name = "Employee APIs", description = "Manage employees")
 public class FirstController {
 	@Autowired
 	FirstService firstService;
 	
-	
+	@Operation(summary = "Get all employees", description = "Returns a list of all employees")
 	@GetMapping("/getemp")
 	public List<Firstprj> getAllData(){
 		return firstService.getAllData();
 	}
 	
-	
+	@Operation(summary = "Add employee", description = "Add a new employee to the database")
 	@PostMapping("/addemp")
 	public Firstprj createData(@RequestBody Firstprj firstprj) {
 		return firstService.createData(firstprj);
 	}
 	
+	@Operation(summary = "Get employee by ID", description = "Fetch employee details using employee ID")
 	@GetMapping("/getemp/{id}")
 	public Firstprj getEmployeeById(@PathVariable Long id) {
 		return firstService.getEmployeeById(id);
